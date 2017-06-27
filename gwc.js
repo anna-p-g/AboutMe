e8847c95a21a1f02db49d5eb2241c468cedad859
anna-p-g <agoetter@g.hmc.edu>
2017-06-27 16:09:41 -0400

    oops

--- gwc.js
+++ gwc.js
@@ -1,5 +1,13 @@
 
 function openTab(tabName){
+  //all tabs
+  tabs = document.getElementsByClassname("tabcontent");
 
-  document.getElementById(tabName).style.display = "block";
+  //hide all tab contents
+  for (i = 0; i < tabs.length; i++){
+    tabs[i].style.display = "none";
+  }
+
+  //display current tab content
+  document.getElementById(tabName).style.display = "inline";
 }
