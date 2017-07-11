e55675afdedc57bdedec13d268bdd09a8b4ef459
anna-p-g <agoetter@g.hmc.edu>
2017-07-11 16:02:43 -0400

    new css

--- gwc.js
+++ gwc.js
@@ -1,11 +1,19 @@
-//var buttons = getElementsByTagName("button");
+var buttons = document.getElementsByTagName("button");
 
 $(document).ready(function(){
-    $("#buttongroup button").click(function(){
-      var i;
-      for (i = 0; i < buttons.length; i++){
-        $(buttons[i]).animate({width: '10%'});
-      }
-        $(this).animate({width: '80%'});
+    $("button").click(function(){
+    var id = $(this);
+    for (i = 0; i < buttons.length; i++){
+    	var button = $(buttons[i]);
+     	if (button.is(id)){
+        	$(id).animate({width: '80%', queue: false});
+          id.style.webkitFilter = "blur(5px)";
+        }
+
+        else{
+    	$(buttons[i]).animate({width: '10%', queue: false});
+		  }
+
+       }
     });
 });
