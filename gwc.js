7945306cc8074b1fd7faad7c507d4934dc028271
anna-p-g <agoetter@g.hmc.edu>
2017-07-13 12:58:58 -0400

    color

--- gwc.js
+++ gwc.js
@@ -6,12 +6,13 @@ $(document).ready(function(){
     for (i = 0; i < buttons.length; i++){
     	var button = $(buttons[i]);
      	if (!(button.is(id))){
-        	$(button).animate({width: '10%'}, {duration: queue: false});
+        	$(button).animate({width: "15%",}, {duration:1000, easing: "swing", queue: false});
           buttons[i].setAttribute("style", "color:default");
         }
 
       else{
-    	   $(id).animate({width: '80%'}, {queue: false});
+    	   $(id).animate({width: "70%"}, {duration: 1000, easing: "swing", queue: false});
+         $(id).fadeTo(1000, 0);
          this.setAttribute("style",  "color: rgba(0, 0, 0, 0)");
 		  }
     }
